375531c0779481931be6e271ee80ef811b2de751
kristian <reciokristian@gmail.com>
2019-04-26 15:49:12 -0500

    login page
    Just a simple form that does nothing.

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Website Title</title>
+    </head>
+    <body>
+        <h1>JAK</h1>
+        <form>
+            Username: <br>
+            <input type="text" /> <br> <br>
+            Password: <br>
+            <input type="password" />
+        </form>
+    </body>
+</html>
