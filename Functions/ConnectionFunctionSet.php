d9ca30189ccc2013d0cb2ad3486af887accccc80
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-05-05 04:36:01 -0500

    Added Comment.

--- Functions/ConnectionFunctionSet.php
+++ Functions/ConnectionFunctionSet.php
@@ -4,6 +4,11 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'jak');
 
+/*
+ * Create a connection with database.
+ * 
+ * Connects to a mysql database using mysqli.
+ */
 function DB_CONNECT() {
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
     if ($conn->connect_errno > 0) {
